b1a946fb2048e005d88d8539e0c265c0d7343a54
cometj03 <harryjung1001@gmail.com>
2024-06-02 00:30:29 +0900

    SP: Project 2 ui

--- Project2/src/VisualSimulator.java
+++ Project2/src/VisualSimulator.java
@@ -6,48 +6,24 @@ import java.io.File;
  * 오직 GUI와 관련 있는 동작만 수행한다.
  */
 public class VisualSimulator extends JFrame {
-    private JPanel MainPanel;
-    private JTextField fileNameTextField;
-    private JButton openButton;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField6;
-    private JTextField textField7;
-    private JTextField textField8;
-    private JTextField textField9;
-    private JTextField textField10;
-    private JTextField textField11;
-    private JTextField textField12;
-    private JTextField textField13;
-    private JTextField textField15;
-    private JTextField textField16;
-    private JTextField textField17;
-    private JTextField textField18;
-    private JTextField textField19;
-    private JTextField textField20;
-    private JTextField textField21;
 
-
-//    String curFileName = null;
-//    JButton fileOpenBtn = new JButton("open");
-//    JButton exitBtn = new JButton("종료");
-//    JButton exeAllBtn = new JButton("실행 (All)");
-//    JButton exeSingleBtn = new JButton("실행 (1 Step)");
-
+    public static void main(String[] args) {
+        VisualSimulator simulator = new VisualSimulator();
+        simulator.setVisible(true);
+        simulator.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
 
     public VisualSimulator() {
         super();
-        init();
+        uiInit();
     }
 
-    private void init() {
+    private void uiInit() {
         this.setSize(500, 700);
         this.setContentPane(MainPanel);
         this.setTitle("SIC/XE Visual Simulator");
 
+        exitButton.addActionListener(e -> dispose());
         openButton.addActionListener(e -> {
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
@@ -57,11 +33,48 @@ public class VisualSimulator extends JFrame {
                 System.out.println(fileChooser.getSelectedFile().getName());
             }
         });
+        exe1StepButton.addActionListener(e -> executeOneStep());
+        exeAllButton.addActionListener(e -> executeAll());
     }
 
-    public static void main(String[] args) {
-        VisualSimulator simulator = new VisualSimulator();
-        simulator.setVisible(true);
-        simulator.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    void executeOneStep() {
+
     }
+
+    void executeAll() {
+
+    }
+
+
+    /* ui 관련 필드 */
+    private JPanel MainPanel;
+    private JTextField tfFileName;
+    private JTextField progNameTF;
+    private JTextField progStartAddrTF;
+    private JTextField progLenTF;
+    private JTextField firstInstAddrTF;
+    private JTextField regDecTF0;
+    private JTextField regDecTF1;
+    private JTextField regDecTF2;
+    private JTextField regDecTF3;
+    private JTextField regDecTF4;
+    private JTextField regDecTF5;
+    private JTextField regDecTF8;
+    private JTextField regHexTF0;
+    private JTextField regHexTF1;
+    private JTextField regHexTF2;
+    private JTextField regHexTF3;
+    private JTextField regHexTF4;
+    private JTextField regHexTF5;
+    private JTextField regHexTF6;
+    private JTextField regHexTF8;
+    private JTextField regHexTF9;
+    private JTextField startAddrMemTF;
+    private JTextField targetAddressTF;
+    private JTextField instructionsTF;
+    private JTextField deviceTF;
+    private JButton openButton;
+    private JButton exe1StepButton;
+    private JButton exeAllButton;
+    private JButton exitButton;
 }
